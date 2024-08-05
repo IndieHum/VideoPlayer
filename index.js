27216bf1d0485e447d104d1fa86aab8e5751b20b
IndieHum <moein.fereyduni@gamil.com>
2024-08-05 19:32:53 +0330

    making pause and play work and some another stuff

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 const TheVideo = document.querySelector("video");
+const PlayButton = document.querySelector(".play-btn");
+const PauseButton = document.querySelector(".pause-btn");
+const NavTime = document.querySelector(".nav-time");
+const NavButton = NavTime.children[0];
 
-console.dir(TheVideo);
+let VideoTime = 10;
 
-document.addEventListener("DOMContentLoaded", () => {
-    setTimeout(() => {
-        TheVideo.play();
-    }, 5000);
-});
+function NavTimeHandler() {
+    setInterval(() => {
+        NavButton.style.left = `${VideoTime}%`;
+        while (TheVideo.currentTime < TheVideo.duration) {
+            NavButton.style.left = `${VideoTime++}%`;
+        }
+    }, TheVideo.duration.toFixed() * 1000);
+}
+
+function PlayHandler() {
+    TheVideo.play();
+    PlayButton.style.display = "none";
+    PauseButton.style.display = "inherit";
+    NavTimeHandler();
+    console.dir(TheVideo.currentTime);
+    console.dir(TheVideo.duration);
+}
+
+function PauseHandler() {
+    TheVideo.pause();
+    PauseButton.style.display = "none";
+    PlayButton.style.display = "inherit";
+}
+
+PlayButton.addEventListener("click", PlayHandler);
+PauseButton.addEventListener("click", PauseHandler);
